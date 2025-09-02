c61fe7d6bff7179db7b6332a14b97191854788a6
ritik-mewada <ritikmewada@gmail.com>
2025-09-01 23:17:39 -0400

    make database connection

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,22 @@
 import app from "./app";
 import config from "config";
 import logger from "./config/logger";
+import { initDb } from "./config/db";
 
-const startServer = () => {
+const startServer = async () => {
     const PORT: number = config.get("server.port");
     try {
+        await initDb();
+        logger.info("Database connected successfully ");
         app.listen(PORT, () => logger.info(`listening on PORT ${PORT}`));
     } catch (err: unknown) {
         if (err instanceof Error) {
             logger.error(err.message);
-            setTimeout(() => {
+            logger.on("finish", () => {
                 process.exit(1);
-            }, 1000);
+            });
         }
     }
 };
 
-startServer();
+void startServer();
