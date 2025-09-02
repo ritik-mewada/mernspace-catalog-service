e89b5be67441a12bff030907f7ca18b79de208c0
ritik-mewada <ritikmewada@gmail.com>
2025-09-02 18:15:54 -0400

    add authentication

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import categoryRouter from "./category/category-router";
 import { globalErrorHandler } from "./common/middlewares/globalErrorhandler";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/categories", categoryRouter);
 
