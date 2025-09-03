dd031a33b3c4a19784d761877d0ec7f40734766d
ritik-mewada <ritikmewada@gmail.com>
2025-09-03 16:06:48 -0400

    image upload to s3

--- src/product/product-router.ts
+++ src/product/product-router.ts
@@ -7,17 +7,27 @@ import { asyncWrapper } from "../common/utils/wrapper";
 import productValidator from "./product-validator";
 import { ProductService } from "./product-service";
 import fileUpload from "express-fileupload";
+import { S3Storage } from "../common/services/S3Storage";
+import createHttpError from "http-errors";
 
 const router = express.Router();
 
 const productService = new ProductService();
-const productController = new ProductController(productService);
+const s3Storage = new S3Storage();
+const productController = new ProductController(productService, s3Storage);
 
 router.post(
     "/",
     authenticate,
     canAccess([Roles.ADMIN]),
-    fileUpload(),
+    fileUpload({
+        limits: { fileSize: 500 * 1024 },
+        abortOnLimit: true,
+        limitHandler: (req, res, next) => {
+            const error = createHttpError(400, "File size exceeds the limit");
+            next(error);
+        },
+    }),
     productValidator,
     asyncWrapper(productController.create),
 );
