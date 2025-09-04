877c833720da2ecd6c54544e85761b4ff6e0f18e
ritik-mewada <ritikmewada@gmail.com>
2025-09-04 12:34:47 -0400

    add cors configuration

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import categoryRouter from "./category/category-router";
 import productRouter from "./product/product-router";
@@ -6,8 +7,27 @@ import { globalErrorHandler } from "./common/middlewares/globalErrorhandler";
 
 const app = express();
 
-app.use(express.json());
+const corsOptions = {
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"],
+    allowedHeaders: [
+        "Origin",
+        "X-Requested-With",
+        "Content-Type",
+        "Accept",
+        "Authorization",
+        "Cache-Control",
+        "X-Auth-Token",
+    ],
+    exposedHeaders: ["X-Auth-Token"],
+    optionsSuccessStatus: 200,
+    preflightContinue: false,
+};
 
+app.use(cors(corsOptions));
+
+app.use(express.json());
 app.use(cookieParser());
 
 app.use("/categories", categoryRouter);
